dce2972c79ea086519c62b76ec70826ab4ed911c
CapaBlanca <blancacapa904@gmail.com>
2024-02-22 10:38:57 -0600

    Imagen click
    asaodisn

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-// En App.js
-import React from 'react';
-import App from './componentes/InicioPeliculas'; // Asegúrate de tener la ruta correcta al componente App
-import './Contenido/InicioPeliculas.css'; // Importa el archivo de estilos si es necesario
 
-function MainApp() {
-  return (
-    <div className="main-app">
+// import React from 'react';
+// import App from './componentes/InicioPeliculas'; // Asegúrate de tener la ruta correcta al componente App
+// import './Estilos/InicioPeliculas.css'; // Importa el archivo de estilos si es necesario
+
+// function MainApp() {
+//   return (
+//     <div className="main-app">
       
-      <App />
-    </div>
+//       <App />
+//     </div>
+//   );
+// }
+
+// export default MainApp;
+
+///////////////////////////////////////////////////////
+
+
+// // En App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import InicioPeliculas from './componentes/InicioPeliculas'; // Importa el componente InicioPeliculas
+import ResenaForm from './componentes/ResenaForm'; // Importa el componente ResenaForm
+import './Estilos/InicioPeliculas.css'; // Importa el archivo de estilos si es necesario
+
+
+function App() {
+  return (
+    <Router>
+      <div className="main-app">
+        <Routes>
+          <Route exact path="/resena" element={<ResenaForm />} />
+          <Route exact path="/" element={<InicioPeliculas />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default MainApp;
+export default App;
--- src/componentes/InicioPeliculas.js
+++ src/componentes/InicioPeliculas.js
@@ -1,12 +1,50 @@
+// import React, { useState } from 'react';
+// import '../Estilos/InicioPeliculas.css'; // Archivo de estilos para la aplicación
+// import imagesData from '../imagenes/Imagenes.js'; // Importa el archivo de datos de imágenes
+
+
+
+
+// function InicioPeliculas() {
+//   const [searchTerm, setSearchTerm] = useState(''); // Estado para el término de búsqueda
+
+//   return (
+//     <div className="app" style={{ backgroundColor: '#030918', color: 'white' }}>
+//       <h1 style={{ color: 'red' }}>TecFlix</h1>
+//       <input
+//         type="text"
+//         placeholder="Buscar en películas"
+//         value={searchTerm}
+//         onChange={(e) => setSearchTerm(e.target.value)}
+//         style={{ color: 'black', backgroundColor: 'white' }}
+//       />
+//       <div className="movies-container">
+       
+//         {/* Mapea sobre el array de datos de imágenes y renderiza cada una */}
+//         {imagesData.map((movie, index) => (
+//           <div key={index} className="movie">
+//             <img src={movie.image} alt={movie.title} />
+//             <h2 style={{ color: 'white' }}>{movie.title}</h2>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default InicioPeliculas;
+
+
+
+
+
 import React, { useState } from 'react';
-import '../Contenido/InicioPeliculas.css'; // Archivo de estilos para la aplicación
-import imagesData from '../imagenes/Imagenes.js'; // Importa el archivo de datos de imágenes
-
-
-
+import { Link } from 'react-router-dom';
+import '../Estilos/InicioPeliculas.css';
+import imagesData from '../imagenes/Imagenes.js';
 
 function InicioPeliculas() {
-  const [searchTerm, setSearchTerm] = useState(''); // Estado para el término de búsqueda
+  const [searchTerm, setSearchTerm] = useState('');
 
   return (
     <div className="app" style={{ backgroundColor: '#030918', color: 'white' }}>
@@ -19,12 +57,13 @@ function InicioPeliculas() {
         style={{ color: 'black', backgroundColor: 'white' }}
       />
       <div className="movies-container">
-        {/* Mapea sobre el array de datos de imágenes y renderiza cada una */}
         {imagesData.map((movie, index) => (
-          <div key={index} className="movie">
-            <img src={movie.image} alt={movie.title} />
-            <h2 style={{ color: 'white' }}>{movie.title}</h2>
-          </div>
+          <Link to="/resenaForm" key={index} className="movie-link">
+            <div className="movie">
+              <img src={movie.image} alt={movie.title} />
+              <h2 style={{ color: 'white' }}>{movie.title}</h2>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
@@ -32,3 +71,4 @@ function InicioPeliculas() {
 }
 
 export default InicioPeliculas;
+
